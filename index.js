991abaac5c6547d16c16ef1da848fb8d81fc1736
mdsalimportfolio <mdsalimalam8@gmail.com>
2023-03-04 22:03:57 +0530

    build

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const jsonServer = require('json-server');
-const cors = require('cors');
-
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-
+const middlewares = jsonServer.defaults();
+const port=process.env.PORT ||3200;
 server.use(jsonServer.defaults());
+server.use(middlewares);
+server.use(router);
 
 server.use(cors({
   origin: 'https://mdsalimprofile.web.app'
 }));
 
-server.use(router);
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running');
 });
